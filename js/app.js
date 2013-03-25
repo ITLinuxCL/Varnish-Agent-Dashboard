b3f8ec7fdbb80a86e16afe971d9a6f31209a6781
Patricio Bruna <pbruna@gmail.com>
2013-03-25 10:53:15 -0300

    first commit

--- js/app.js
+++ js/app.js
@@ -1,15 +1,26 @@
 window.App = {};
 App.refreshTime = 2000;
+App.oldStats = {};
 App.newStats = {};
 
 App.getStats = function(){
 	$.getJSON("/stats", function(data){
+		App.oldStats = App.newStats;
 		App.newStats = data;
 	})
 }
 
+App.updateHitRatio = function({
+	$("#hit-ratio").text(App.newStats.cache_hit.value);
+});
+
+App.updateData = function({
+	App.getStats;
+	App.updateHitRatio();
+});
+
 $(function(){
 
-	setInterval(App.getStats,App.refreshTime);
+	setInterval(App.updateData,App.refreshTime);
 	
 });
