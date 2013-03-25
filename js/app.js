d46fece12d116cb11f1ad6b1deedac1c0952f0bc
Patricio Bruna <pbruna@gmail.com>
2013-03-25 09:52:29 -0300

    first commit

--- js/app.js
+++ js/app.js
@@ -1,32 +1,24 @@
-window.VarnishDashboard = Ember.Application.create({});
+window.App = Ember.Application.create({});
 
-VarnishDashboard.IndexRoute = Ember.Route.extend({
+App.IndexRoute = Ember.Route.extend({
   setupController: function(controller) {
     controller.set('content', ['red', 'yellow', 'blue']);
   }
 });
 
-VarnishDashboard.ApplicationController = Ember.Controller.extend({
+App.ApplicationController = Ember.Controller.extend({
 	first: "Tom",
 	last: "Brand"
 });
 
-VarnishDashboard.Store = DS.Store.extend({
+App.Store = DS.Store.extend({
 	revision: 11,
 	url: "http://localhost:6085",
 	adapter: "DS.RESTAdapter"
 });
 
 
-ClientConn = DS.Model.extend({
-	value: DS.attr('number'),
-	flag: DS.attr('string'),
-	description: DS.attr('string'),
-});
-
-VarnishDashboard.Stat = DS.Model.extend({
-	timestamp: DS.attr('date'),
-	clientConn: DS.hasOne(ClientConn, {embedded: true, key: "client_conn"})
-	
+App.Stat = DS.Model.extend({
+	timestamp: DS.attr('string'),	
 });
 
