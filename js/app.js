d26b418d1cdc3e618ff3e1178f5580f528a64077
Patricio Bruna <pbruna@gmail.com>
2013-03-25 15:56:37 -0300

    first commit

--- js/app.js
+++ js/app.js
@@ -26,6 +26,41 @@ App.updateHitRatio = function(){
 App.updateData = function(){
 	App.getStats();
 	App.updateHitRatio();
+	App.builMetricsTable("cache", App.getCacheMetrics());
+	App.builMetricsTable("traffic", App.getCacheMetrics());
+	App.builMetricsTable("backend", App.getCacheMetrics());
+}
+
+App.getCacheMetrics = function() {
+	var hits_ratio = {
+		label: "Hits Ratio",
+		new_value: App.calcHitRatio(),
+		average_value: App.calcHitRatio()
+	}
+	var hits_qty = {
+		label: "Hits Qty.",
+		new_value: App.newStats.cache_hit.value,
+		average_value: App.oldStats.cache_hit.value
+	}
+	var miss_qty = {
+		label: "Miss Qty.",
+		new_value: App.newStats.cache_miss.value,
+		average_value: App.oldStats.cache_miss.value
+	}
+	var obj_cache = {
+		label: "Objs. in Cache",
+		new_value: App.newStats.n_object.value,
+		average_value: App.oldStats.n_object.value
+	}
+	return [hits_ratio, hits_qty, miss_qty, obj_cache]
+}
+
+App.builMetricsTable = function(table_id, values_object){
+	var source   = $("#metrics-table-template").html();
+	var context = { metric: values_object};
+	var template = Handlebars.compile(source);
+	var html = template(context);
+	$("#"+table_id+"-metrics-table").html(html);
 }
 
 $(function(){
