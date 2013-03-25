4c731720367d061daee6f7472effa756400ee677
Patricio Bruna <pbruna@gmail.com>
2013-03-25 11:06:30 -0300

    first commit

--- js/app.js
+++ js/app.js
@@ -11,7 +11,9 @@ App.getStats = function(){
 }
 
 App.calcHitRatio = function(){
-	hitRatio = (App.newStats.cache_hit.value*100)/App.newStats.client_req.value;
+	delta_client_req = App.newStats.client_req.value - App.oldStats.client_req.value;
+	delta_cache_hit = App.newStats.cache_hit.value - App.oldStats.cache_hit.value;
+	hitRatio = (delta_cache_hit*100)/delta_client_req.value;
 	return hitRatio;
 }
 
