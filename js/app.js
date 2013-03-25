6b81b79eaa42036085a8500b1ae3e39a718cdb82
Patricio Bruna <pbruna@gmail.com>
2013-03-25 18:51:09 -0300

    first commit

--- js/app.js
+++ js/app.js
@@ -39,7 +39,7 @@ App.getCacheMetrics = function() {
 	}
 	var hits_qty = {
 		label: "Hits Qty.",
-		new_value: nFormatter(App.newStats.cache_hit.value - App.oldStats.cache_hit.value),
+		new_value: App.newStats.cache_hit.value - App.oldStats.cache_hit.value,
 		average_value: nFormatter(App.newStats.cache_hit.value / App.refreshTime)
 	}
 	var miss_qty = {
