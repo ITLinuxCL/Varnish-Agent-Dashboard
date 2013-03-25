a314ef7366fc24f80da3301d829d7c6e1bcc2bb0
Patricio Bruna <pbruna@gmail.com>
2013-03-25 10:42:02 -0300

    first commit

--- js/app.js
+++ js/app.js
@@ -1,24 +1,9 @@
-window.App = Ember.Application.create({});
+window.App = {};
+App.newStats = {};
 
-App.IndexRoute = Ember.Route.extend({
-  setupController: function(controller) {
-    controller.set('content', ['red', 'yellow', 'blue']);
-  }
-});
-
-App.ApplicationController = Ember.Controller.extend({
-	first: "Tom",
-	last: "Brand"
-});
-
-App.Store = DS.Store.extend({
-	revision: 11,
-	url: "http://localhost:6085",
-	adapter: "DS.RESTAdapter"
-});
-
-
-App.Stat = DS.Model.extend({
-	timestamp: DS.attr('string'),	
-});
+App.getStats = function(){
+	$.getJSON("/stats", function(data){
+		newStats = data;
+	})
+}
 
