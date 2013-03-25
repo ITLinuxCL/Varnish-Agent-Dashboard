7d5838bfbfb013c489867d2e860d977eb8649df8
Patricio Bruna <pbruna@gmail.com>
2013-03-25 09:44:17 -0300

    first commit

--- js/app.js
+++ js/app.js
@@ -17,15 +17,16 @@ VarnishDashboard.Store = DS.Store.extend({
 	adapter: "DS.RESTAdapter"
 });
 
-VarnishDashboard.Stat = DS.Model.extend({
-	timestamp: DS.attr('date'),
-	clientConn: DS.attr(ClientConn, {embedded: true, key: "client_conn"})
-	
-});
-
 
 ClientConn = DS.Model.extend({
 	value: DS.attr('number'),
 	flag: DS.attr('string'),
 	description: DS.attr(string),
 });
+
+VarnishDashboard.Stat = DS.Model.extend({
+	timestamp: DS.attr('date'),
+	clientConn: DS.attr(ClientConn, {embedded: true, key: "client_conn"})
+	
+});
+
